989957b58fcabdda0106000749577c65abd8d19f
shaxa9505 <87860218+shaxa9505@users.noreply.github.com>
2021-12-20 19:28:43 +0500

    js7
    js7

---
+++ Js 7-dars/script.js
@@ -0,0 +1,46 @@
+// 7-dars
+
+// var arr = [
+//     "dushanba",
+//     "seshanba",
+//     "seshanba",
+//     "chorshanba",
+//     "payshanba",
+//     "shanba",
+//     "yakshanba",
+// ]
+
+// for(var i = 0; i < arr.length; i++){
+//     if(i == 5 || i == 6){
+//         document.write(`<b> ${arr[i]} <br>`)
+//     } else {
+//         document.write(`${arr[i]} <br>`)
+//     }
+// }
+
+// var arr = [1, 3, 9, -11, -22, -33];
+// var newArr = [];
+
+// function ispos (arr){
+//     if(arr > 0){
+//         return true
+//     } else{
+//         return false
+//     }
+// }
+
+// for(var i = 0; i < arr.length; i++){
+//     if(ispos(arr[i])){
+//         newArr.push(arr[i])
+//     }
+// }
+// console.log(newArr);
+
+
+// var arr = [1, 2, 3, 4, 5];
+// var op = 0;
+
+// for(var i = 0; i < arr.length; i++){
+//     op += arr[i]
+// }
+// console.log(op);
